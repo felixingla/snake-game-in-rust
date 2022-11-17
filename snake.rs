a8b90b659b398de0243b94391defde74cb8f0a86
fingla <79790608+fingla@users.noreply.github.com>
2022-11-17 21:30:54 +0100

    Add files via upload

---
+++ snake.rs
@@ -0,0 +1,28 @@
+// ~/src/snake.rs
+
+use crate::direction::Direction;
+use crate::point::Point;
+
+#[derive(Debug)]
+pub struct Snake {
+    body: Vec<Point>,
+    direction: Direction,
+    digesting: bool,
+}
+
+impl Snake {
+    pub fn new(start: Point, length: u16, direction: Direction) -> Self {
+        let opposite = direction.opposite();
+        let body: Vec<Point> = (0..length)
+            .into_iter()
+            .map(|i| start.transform(opposite, i))
+            .collect();
+
+        Self { body, direction, digesting: false }
+    }
+
+    pub fn get_head_point(&self) -> Point {
+        self.body.first().unwrap().clone()
+    }
+
+}
